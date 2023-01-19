10bb10eabc7fb0dc79145eda3970201d3b4d5e20
Galo Vega <contacto.detodounpocomx@gmail.com>
2023-01-18 22:37:06 -0600

    solved some modal problems

--- script.js
+++ script.js
@@ -4,16 +4,6 @@ const consentForm = document.getElementById("consent-form")
 
 
 setTimeout(function(){
-    modalInput.style.display= "flex"
-},7400)
+    modal.style.display= "none"
+},2800)
 
-consentForm.addEventListener('submit', function(e){
-    e.preventDefault()
-    const consentFormData = new FormData(consentForm)
-    const name = consentFormData.get("userName")
-    modal.style.display = "none"
-    document.getElementById("main-info").innerHTML = 
-    ` 
-    <h1>Hi ${name}!<br> <span>My name is Galo</span> </h1>
-    `
-}) 
